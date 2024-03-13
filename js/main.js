a80fbb639425b0620ecf9fd8f7a6f9f0553602ac
ahmedkilany123 <97148391+ahmedkilany123@users.noreply.github.com>
2024-03-13 11:05:46 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
-
 let listNav = document.querySelectorAll("header nav ul li");
 
-listNav.forEach(function(el) {
-
-    el.onclick = function () {
-
-        listNav.forEach (function(el)  {
-            el.classList.remove("active");
-        });
-        this.classList.add("active");
-    }
+listNav.forEach(function (el) {
+  el.onclick = function () {
+    listNav.forEach(function (el) {
+      el.classList.remove("active");
+    });
+    this.classList.add("active");
+  };
 });
 
 let srchIcon = document.getElementById("srchIcon");
 
 srchIcon.onclick = function () {
-    if (srchIcon.classList.contains("active") ) {
-        srchIcon.classList.remove("active");
-    } else {
+  if (srchIcon.classList.contains("active")) {
+    srchIcon.classList.remove("active");
+    logBox.classList.remove("active");
+  } else {
     srchIcon.classList.add("active");
-    }
-}
-
+    logBox.classList.remove("active");
+  }
+};
 
 let login = document.getElementById("login");
 let logBox = document.querySelector(".left .logBox");
 
 login.onclick = function () {
-    if (login.classList.contains("active") ) {
-        login.classList.remove("active");
-        logBox.classList.remove("active");
-    } else {
+  if (login.classList.contains("active")) {
+    login.classList.remove("active");
+    logBox.classList.remove("active");
+  } else {
     login.classList.add("active");
     logBox.classList.add("active");
-    }
-}
+    srchIcon.classList.remove("active");
+  }
+};
